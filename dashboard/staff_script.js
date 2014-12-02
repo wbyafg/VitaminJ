ccd00e34a73c7fc9e4cede21e51c129e1469992f
Xinynag Li <lixinyang1026@gmail.com>
2014-12-02 18:50:38 +0800

    add apply leave page html
    add apply html only for dashboard_staff for testing purpose, because
    the form will be generated by jQuery at realtime later. nav-bar
    selection issue solved

--- dashboard/staff_script.js
+++ dashboard/staff_script.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
-    $("li").click(function () {
-        var listItems = $("#navbar li");
-        listItems.each(function () {
-            $(this).removeClass("active");
-        });
 
-        $("li").click(function () {
-            $(this).addClass("active");
+    //switch navigation side-bar
+    $('.nav-sidebar > li').click(function () {
+        $('.nav-sidebar li').each(function () {
+            $(this).removeClass('active');
         });
+        $(this).addClass('active');
+        console.log(this);
     });
+});
+
+$(document).ready(function () {
+    $('#myTab a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
 });
